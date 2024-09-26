54d8a2dc7ab12c4165ec309fd51e6865fc432520
Tony133 <a.tripodi133@gmail.com>
2024-09-26 21:54:14 +0200

    chore: update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://mercurius.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -61,8 +61,6 @@ const config: Config = {
     ]
   ],
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
     navbar: {
       logo: {
         alt: "Mercurius Logo",
